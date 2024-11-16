dca5807659165487dcd2b1abd759d8ddb650d4a6
Kevin Ernest Long <kevinelong@gmail.com>
2024-11-16 15:24:15 -0800

    first commit

---
+++ app_data.js
@@ -0,0 +1,21 @@
+const app_data = {
+    users: [
+        {
+            name: "default", pw: "1234", decks: [
+                {
+                    name: "Quiz 1",
+                    cards: [
+                        {
+                            q: "pi",
+                            a: "3.14159"
+                        },
+                        {
+                            q: "fi",
+                            a: "1.62549"
+                        },
+                    ]
+                }
+            ]
+        }
+    ]
+}; // end app_data
